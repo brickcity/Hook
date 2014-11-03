b4814ed951139a1093c512725909bb8890c1d208
curtismitchell <curtismitchell@gmail.com>
2014-11-03 14:34:46 -0500

    distinguished 'inspect' view

--- Hook/Controllers/HooksController.cs
+++ Hook/Controllers/HooksController.cs
@@ -10,16 +10,35 @@ namespace Hook.Controllers
     public class HooksController : Controller
     {
         // GET: Hooks
-        [Route("{id?}")]
-        public ActionResult Index(string id)
+        [Route("{id?}", Name = "detail")]
+        public ActionResult Index(string id, string inspect)
         {
             if(String.IsNullOrEmpty(id)) return View();
+
+            //todo: make sure the id exists
+            if (this.InInspectMode()) return this.InspectView(id);
 
             return this.Detail(id);
         }
 
+        private bool InInspectMode()
+        {
+            if (Request.QueryString.Count == 0) return false;
+            var qs = Request.QueryString[0];
+            if (qs == null) return false;
+            return (qs.Equals("inspect", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private ViewResult InspectView(string id)
+        {
+            ViewBag.Id = id;
+            ViewBag.Url = Url.RouteUrl("detail", new { id }, "http");
+            return this.View("Inspect");
+        }
+
         private ViewResult Detail(string id)
         {
+            
             ViewData.Model = id;
             return this.View("Detail");
         }
@@ -28,7 +47,7 @@ namespace Hook.Controllers
         public ActionResult New()
         {
             var id = new HookId().ToString();
-            return RedirectToAction("Index", new { Id = id });
+            return this.Redirect(string.Format("/{0}?inspect", id));
         }
     }
 }
